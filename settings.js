bd02f5e9aaad06a19486450cda154eff3951d6df
queenrashu136-hue <queenrashu136@gmail.com>
2025-11-20 19:51:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RASHU-MD=WR930QiD#YQvWVisPgDxIh_kx_b90SGMn6Y6fKVQ2VuPNhAyhhws",
+SESSION_ID: process.env.SESSION_ID || "RASHU-MD=OYFXUCTJ#TSnOvLLfebSSvtgh5198hrSY_yI9lk3CPj5g7xn3bSk",
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
